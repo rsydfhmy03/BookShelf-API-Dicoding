89bb4d86bcd8632744a745870d0a0a58ba8d30d0
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2024-04-30 19:24:01 +0700

    refactor code addBook, getIndex, ShowAllBook

--- src/handler/getIndex.js
+++ src/handler/getIndex.js
@@ -1,24 +1,24 @@
 /**
  * Returns a response object containing a welcome message and information about the Bookshelf API.
  *
- * @param {Object} request - the request object
- * @param {Object} h - the response toolkit object
- * @return {Object} a response object with a success status, a welcome message, and information about the API
+ * @param {Object} request - The request object.
+ * @param {Object} h - The response toolkit object.
+ * @return {Object} A response object with a success status and information about the API.
  */
 const getIndex = (request, h) => {
-  const response = h.response({
-    status: "success",
-    message: "Welcome to Bookshelf API Dicoding Submission",
-    data: {
-      name: "Bookshelf API",
-      version: "1.0.0",
-      author: "Mitahudev03 (Fahmy Rosyadi CC-44)",
-      status: "Active Student",
-      school: "Politeknik Negeri Jember",
-    },
-  });
-  response.code(200);
-  return response;
+  return h
+    .response({
+      status: "success",
+      message: "Welcome to Bookshelf API Dicoding Submission",
+      data: {
+        name: "Bookshelf API",
+        version: "1.0.0",
+        author: "Mitahudev03 (Fahmy Rosyadi CC-44)",
+        status: "Active Student",
+        school: "Politeknik Negeri Jember",
+      },
+    })
+    .code(200);
 };
 
 module.exports = getIndex;
